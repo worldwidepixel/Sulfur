3cb6af1bf342260f114521213a046b6f825322bd
AshyBoxy <ashyboxy@ashyboxy.xyz>
2024-05-24 03:54:50 +0100

    Implement a very important sophisticated algorithm

--- src/main/java/ca/worldwidepixel/sulfur/ClientInit.java
+++ src/main/java/ca/worldwidepixel/sulfur/ClientInit.java
@@ -1,10 +1,28 @@
 package ca.worldwidepixel.sulfur;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class ClientInit implements ClientModInitializer {
+    List<Integer> sortList1 = new ArrayList<>();
+    List<Integer> sortList2 = new ArrayList<>();
+    Random r = new Random();
+
     @Override
     public void onInitializeClient() {
-
+        for (int i = 0; i < 2000; i++) {
+            sortList1.add(r.nextInt());
+        }
+        for (int i = 0; i < 8000; i++) {
+            sortList2.add(r.nextInt());
+        }
+        WorldRenderEvents.START.register((context -> {
+            if (r.nextInt(120) == 1) BubbleSort.sort(sortList2);
+            else BubbleSort.sort(sortList1);
+        }));
     }
 }
